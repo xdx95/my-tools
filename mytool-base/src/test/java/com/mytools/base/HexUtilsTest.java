4b5dd9bee9e80c4d7e903b29d32e7ac5cf3464a0
xdx <xingdengxiang@outlook.com>
2024-10-30 15:50:22 +0800

    update: 结构调整

--- mytool-base/src/test/java/com/mytools/base/HexUtilsTest.java
+++ mytool-base/src/test/java/com/mytools/base/HexUtilsTest.java
@@ -17,8 +17,6 @@ public class HexUtilsTest {
 
 	public static final String HEX_EXPAND_DATA = "31 32 33 34 35";
 
-
-
 	@Test
 	public void encodeToHex() {
 		String hexStr = HexUtils.encodeToHex(DATA.getBytes(StandardCharsets.UTF_8));
