bde340cb7b2cfcaa703afde063568028f96ef2b0
xdx <xingdengxiang@outlook.com>
2024-10-31 11:03:49 +0800

    test: 新增 Base64UtilsTest

---
+++ mytool-base/src/test/java/com/mytools/base/Base64UtilsTest.java
@@ -0,0 +1,16 @@
+package com.mytools.base;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Base64UtilsTest {
+
+	@Test
+	public void encode() {
+	}
+
+	@Test
+	public void decode() {
+	}
+}
