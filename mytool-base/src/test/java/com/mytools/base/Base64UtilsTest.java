05f4733261a7a6f0a8e88db2b7129b72605b4432
xdx <xingdengxiang@outlook.com>
2024-11-01 13:46:24 +0800

    test: git-1

--- mytool-base/src/test/java/com/mytools/base/Base64UtilsTest.java
+++ mytool-base/src/test/java/com/mytools/base/Base64UtilsTest.java
@@ -1,15 +1,12 @@
 package com.mytools.base;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class Base64UtilsTest {
 
 	@Test
 	public void encode() {
-		System.out.println("git test");
-
+		System.out.println("第一次提交 git test");
 	}
 
 	@Test
