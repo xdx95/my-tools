f038e27c70b49fa2515e8972015487568c852d74
xdx <xingdengxiang@outlook.com>
2024-11-22 16:01:40 +0800

    build: 构建方式调整

--- tools-base/src/main/java/com/my/tools/base/VersionUtils.java
+++ tools-base/src/main/java/com/my/tools/base/VersionUtils.java
@@ -11,27 +11,30 @@ public class VersionUtils {
 
 	private static final String DEFAULT_VERSION = "unknown";
 
-	public static String getVersion(Class<?> clazz, String groupId, String artifactId) {
+	/**
+	 * 兼容本地发开和正式发布的 jar 包
+	 */
+	public static String getVersion(Class<?> clazz) {
 		String version = getVersionFromPackage(clazz);
 		if (DEFAULT_VERSION.equals(version)) {
-			version = getVersionFromPom(clazz, groupId, artifactId);
-		}
-		if (DEFAULT_VERSION.equals(version)) {
 			version = getVersionFromProperties();
-		}
-		if (DEFAULT_VERSION.equals(version)) {
-			version = getVersionFromSystemProperty();
 		}
 		return version;
 	}
 
-	private static String getVersionFromPackage(Class<?> clazz) {
+	/**
+	 * META-INF/MANIFEST.MF 文件
+	 */
+	public static String getVersionFromPackage(Class<?> clazz) {
 		Package pkg = clazz.getPackage();
 		return (pkg != null && pkg.getImplementationVersion() != null)
 			? pkg.getImplementationVersion() : DEFAULT_VERSION;
 	}
 
-	private static String getVersionFromPom(Class<?> clazz, String groupId, String artifactId) {
+	/**
+	 * META-INF/maven/groupId/artifactId/pom.properties 文件
+	 */
+	public static String getVersionFromPom(Class<?> clazz, String groupId, String artifactId) {
 		String resourcePath = String.format("/META-INF/maven/%s/%s/pom.properties", groupId,
 			artifactId);
 		try (InputStream input = clazz.getClassLoader().getResourceAsStream(resourcePath)) {
@@ -45,7 +48,10 @@ public class VersionUtils {
 		return DEFAULT_VERSION;
 	}
 
-	private static String getVersionFromProperties() {
+	/**
+	 * 自定义 version.properties
+	 */
+	public static String getVersionFromProperties() {
 		try (InputStream input = VersionUtils.class.getResourceAsStream("/version.properties")) {
 			if (input != null) {
 				Properties props = new Properties();
@@ -57,7 +63,10 @@ public class VersionUtils {
 		return DEFAULT_VERSION;
 	}
 
-	private static String getVersionFromSystemProperty() {
+	/**
+	 * 系统启动参数
+	 */
+	public static String getVersionFromSystemProperty() {
 		return System.getProperty("project.version", DEFAULT_VERSION);
 	}
 }
